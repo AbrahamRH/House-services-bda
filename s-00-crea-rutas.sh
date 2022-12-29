c0f860aef2fc3bedf8423ec495b292a9d102bb62
AbrahamRH <abrahamrzhz@gmail.com>
2022-12-29 13:01:38 -0600

    Creación del script para hacer las rutas

---
+++ s-00-crea-rutas.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+# @Authors:
+# @Fecha:
+# @Descripción:
+
+
+dirProyecto=/unam-bda/Proyecto
+
+
+echo "Creando carpeta raíz proyecto/"
+mkdir -p ${dirProyecto}
+
+
+echo "Creando discos"
+mkdir -p ${dirProyecto}/d01
+mkdir -p ${dirProyecto}/d02
+mkdir -p ${dirProyecto}/d03
+mkdir -p ${dirProyecto}/d04
+mkdir -p ${dirProyecto}/d05
+
+
