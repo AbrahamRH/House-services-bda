e074649144cfe45aefbbc6be9126481955272023
AbrahamRH <abrahamrzhz@gmail.com>
2022-12-30 12:37:21 -0600

    Agregando script para ejecutar los scripts del diccionario de datos

---
+++ s-01-crea-diccionario.sql
@@ -0,0 +1,14 @@
+
+-- @Autores:
+-- @Fecha: 
+-- @Descripción:
+
+connect sys as sysdba
+
+
+@?/rdbms/admin/catalog.sql
+@?/rdbms/admin/catproc.sql
+@?/rdbms/admin/utlrp.sql
+
+connect system/system2
+@?/sqlplus/admin/pupbld.sql
