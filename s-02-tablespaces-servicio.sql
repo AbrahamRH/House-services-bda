ec2561e1a8ec7ddf680b4ddfb6168d59b125db7f
AbrahamRH <abrahamrzhz@gmail.com>
2023-01-15 11:36:02 -0600

    Haciendo cambios de las rutas en los datafiles

--- s-02-tablespaces-servicio.sql
+++ s-02-tablespaces-servicio.sql
@@ -1,30 +1,30 @@
 
 
 create tablespace servicio_multiple_tbs
-datafile '/unam-bda/proyecto/d09/servicio_multiple_tbs01.dbf' size 10m,
-         '/unam-bda/proyecto/d09/servicio_multiple_tbs02.dbf' size 10m,
-         '/unam-bda/proyecto/d10/servicio_multiple_tbs03.dbf' size 10m
+datafile '/unam-bda/proyecto/d09/app/oracle/oradata/RALUPROY/servicio_multiple_tbs01.dbf' size 10m,
+         '/unam-bda/proyecto/d09/app/oracle/oradata/RALUPROY/servicio_multiple_tbs02.dbf' size 10m,
+         '/unam-bda/proyecto/d10/app/oracle/oradata/RALUPROY/servicio_multiple_tbs03.dbf' size 10m
 extent management local autoallocate
 segment space management auto;
 
 
 create encrypt tablespace servicio_encrypt_tbs
-datafile '/unam-bda/proyecto/d09/servicio_encrypt_tbs01.dbf' size 10m
+datafile '/unam-bda/proyecto/d09/app/oracle/oradata/RALUPROY/servicio_encrypt_tbs01.dbf' size 10m
 extent management local autoallocate
 segment space management auto;
 
 
 create temporary tablespace servcio_temp_tbs
-datafile '/unam-bda/proyecto/d09/servicio_temp_tbs01.dbf' size 20m
+datafile '/unam-bda/proyecto/d09/app/oracle/oradata/RALUPROY/servicio_temp_tbs01.dbf' size 20m
 reuse;
 
 
 create undo tablespace servicio_undo_tbs
-datafile '/unam-bda/proyecto/d09/servicio_undo_tbs01.dbf' size 20m
+datafile '/unam-bda/proyecto/d09/app/oracle/oradata/RALUPROY/servicio_undo_tbs01.dbf' size 20m
 reuse;
 
 create tablespace servicio_indexes_tbs
-datafile '/unam-bda/proyecto/d09/servicio_indexes_tbs01.dbf' size 20m
+datafile '/unam-bda/proyecto/d09/app/oracle/oradata/RALUPROY/servicio_indexes_tbs01.dbf' size 20m
 extent management local autoallocate
 segment space management auto;
 
