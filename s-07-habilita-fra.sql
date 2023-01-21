5678b9619477dd1b68c4e3fd5ea1269560b59f4c
AbrahamRH <abrahamrzhz@gmail.com>
2023-01-21 14:29:59 -0600

    Cambiando los parametros de la FRA

--- s-07-habilita-fra.sql
+++ s-07-habilita-fra.sql
@@ -2,11 +2,11 @@
 connect sys/system2 as sysdba
 
 
-alter system set db_recovery_file_dest_size=3g scope=spfile;
+alter system set db_recovery_file_dest_size=20g scope=spfile;
 
 alter system set db_recovery_file_dest='/unam-bda/proyecto/d06/app/oracle/oradata/RALUPROY/FRA' scope=spfile;
 
-alter system set db_flashback_retention_target=30 scope=spfile;
+alter system set db_flashback_retention_target=720 scope=spfile;
 
 shutdown immediate
 startup
---
+++ s-08-crea-grupos-fra.sql
@@ -0,0 +1,7 @@
+
+
+connect sys/system2 as sysdba
+
+
+alter database
+  add logfile size 50m;
