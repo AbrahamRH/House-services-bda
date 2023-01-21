19f5d2dfa5211b0c3354812157e168a6be833d0d
AbrahamRH <abrahamrzhz@gmail.com>
2023-01-21 14:08:25 -0600

    Se habilito la fra y el modo archive en la misma

--- s-05-modo-archivado.sql
+++ s-05-modo-archivado.sql
@@ -3,7 +3,7 @@ connect sys/system2 as sysdba
 alter system set log_archive_max_processes = 2 scope = spfile;
 alter system set log_archive_dest_1 = 'LOCATION=/unam-bda/proyecto/d04/app/oracle/oradata/RALUPROY MANDATORY' scope = spfile;
 alter system set log_archive_dest_2 = 'LOCATION=/unam-bda/proyecto/d05/app/oracle/oradata/RALUPROY' scope = spfile;
-alter system set log_archive_dest_3 = 'LOCATION=/unam-bda/proyecto/d06/app/oracle/oradata/RALUPROY' scope = spfile;
+alter system set log_archive_dest_3 = 'LOCATION=USE_DB_RECOVERY_FILE_DEST' scope = spfile;
 alter system set log_archive_format = 'arch_raluproy_%t_%s_%r.arc' scope = spfile;
 alter system set log_archive_min_succeed_dest = 1 scope = spfile;
 
